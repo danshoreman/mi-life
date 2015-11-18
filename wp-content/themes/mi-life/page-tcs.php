9aa97f879a404e9d0c65a5c9ae9c6fcb12a9dcc5
Dan Shoreman <dan@danshoreman.co.uk>
2015-11-18 19:27:41 +0000

    Removing old templates & adding tcs template page

--- wp-content/themes/mi-life/page-tcs.php
+++ wp-content/themes/mi-life/page-tcs.php
@@ -1,63 +1,42 @@
-<?php 
-
+<?php get_header(); 
 /*
- * The template for Everything
- */
-
-get_header(); ?>
-	
+Template Name: T&Cs
+*/
+?>
+<div class="page-tcs">
 	<div class="row">
-	
-		<div class="small-12 columns" role="main">
+		<div class="small-12 medium-10 medium-offset-1 large-8 large-offset-2 columns" role="main">
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-				
+
 				<header class="article-header">
-					
-					<h1><?php the_title(); ?></h1>
-				
+					<h2 itemprop="headline"><?php the_title(); ?></h2>
 				</header>
-				
-				<section class="entry-content">
-					
+
+				<section class="entry-content" itemprop="articleBody">
 					<?php the_content(); ?>
-				
 				</section>
-				
+
 			</article>
-			
-			<?php endwhile; ?>
-			
-			<?php else : ?>
-			
+
+			<?php endwhile; else : ?>
+
 			<article class="post-not-found">
-				
+			
 				<header class="not-found-header">
-					
 					<h2><?php _e( 'Nothing Found!' ); ?></h2>
-				
 				</header>
 				
 				<section class="not-found-content">
-					
-					<p><?php _e( 'Please check what you are looking for' ); ?></p>
-				
+					<p><?php _e( 'Please check what you are looking for.' ); ?></p>
 				</section>
 			
 			</article>
-			
+
 			<?php endif; ?>
-			
-			<div class="below-nav">
-				
-				<?php posts_nav_link(' - ', '&laquo; Prev', 'Next &raquo;'); ?>
-			
-			</div>
-		
 		</div>
-
 	</div>
-
-<?php get_footer();
+</div>
+<?php get_footer(); ?>
